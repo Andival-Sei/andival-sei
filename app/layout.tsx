2bede74a6cf40371faa02886ef9e0a96a50179e6
Kirill Barkov <freedomdragon777@gmail.com>
2025-11-22 22:59:37 +0400

    docs: добавить документацию по конвенции коммитов и обновить README
    - Добавлен новый файл документации о правилах оформления коммитов
    - Обновлен README с ссылкой на новую документацию
    - Изменен язык в layout.tsx на русский

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
